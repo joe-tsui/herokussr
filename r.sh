48c5dceb39bcd76851aa3f2e37f2013886375d39
joe-tsui <xuqiang4546@gmail.com>
2018-04-22 08:24:46 +0800

    Update r.sh

--- r.sh
+++ r.sh
@@ -1,3 +1,5 @@
+rm -rf /etc/localtime
+ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 sed -i "s/myport/$PORT/g" /config.json
 sed -i "s/mypassword/$PASSWORD/g" /config.json
 python server.py -c /config.json > /dev/null
